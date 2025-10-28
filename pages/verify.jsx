bfc63dfe17c121acf21fa9dddf6f921f014b84c1
Ange <ishimwenzizaangell@gmail.com>
2025-10-28 17:19:08 +0200

    .

---
+++ pages/verify.jsx
@@ -0,0 +1,11 @@
+import { View,Text,TouchableOpacity } from "react-native-web"
+
+export default VerifyScreen({navigation}){
+       <View style={Styles.container}>
+          <View style={styles.ImageContainer}>
+           <Image source ={require("../assets/images/cuate.png")}
+                  
+              </View>
+        <Text style={styles.mainHeading}>Enter OTP</Text>
+          </View>
+}
